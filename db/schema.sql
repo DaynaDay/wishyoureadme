d0ea4225576ef2beb65ae4b43330d5cec65022fd
Dayna Knight <dknighted11@gmail.com>
2020-03-12 16:26:22 -0400

    created schema and seeds file.

---
+++ db/schema.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE book_wish_db;
+USE book_wish_db;
+
+CREATE TABLE poetry
+(
+	id int NOT NULL AUTO_INCREMENT,
+	author varchar(255) NOT NULL,
+	title VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE sci-fi
+(
+	id int NOT NULL AUTO_INCREMENT,
+	author varchar(255) NOT NULL,
+	title VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE romance
+(
+	id int NOT NULL AUTO_INCREMENT,
+	author varchar(255) NOT NULL,
+	title VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE biographical
+(
+	id int NOT NULL AUTO_INCREMENT,
+	author varchar(255) NOT NULL,
+	title VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE mystery
+(
+	id int NOT NULL AUTO_INCREMENT,
+	author varchar(255) NOT NULL,
+	title VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
