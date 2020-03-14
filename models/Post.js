9d91039b620709d1aca99332eef4d6af7d1cc354
Dayna Knight <dknighted11@gmail.com>
2020-03-14 10:47:50 -0400

    worked on post model file

--- models/Post.js
+++ models/Post.js
@@ -1,26 +1,42 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 // create our Post model
-class Post extends Model {}
+class Post extends Model { }
 
 // create fields/columns for Post model
 Post.init(
   {
     title: {
       type: DataTypes.STRING,
-      allowNull: false
-    },
-    body: {
-      type: DataTypes.TEXT,
       allowNull: false,
       validate: {
         len: [1]
       }
-    }
-  },
+    },
+
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+  
+
+
+
+
   {
     sequelize
   }
-);
+  );
 
 module.exports = Post;
